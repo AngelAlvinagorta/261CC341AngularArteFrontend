372c70e113fe9a9bb2930777407fc7821e56ba8d
Angel Alvinagorta <angelalvinagorta@gmail.com>
2026-07-01 03:59:04 -0500

    Cambios necesarios para pasar el conjunto de pruebas unitarias

--- src/app/components/registrar-obra/registrar-obra.spec.ts
+++ src/app/components/registrar-obra/registrar-obra.spec.ts
@@ -1,18 +1,29 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { RegistrarObra } from './registrar-obra';
+import { provideHttpClient } from '@angular/common/http';
+import { provideRouter } from '@angular/router';
+import { vi } from 'vitest';
 
 describe('RegistrarObra', () => {
   let component: RegistrarObra;
   let fixture: ComponentFixture<RegistrarObra>;
 
   beforeEach(async () => {
+    // Sintaxis de Vitest para espiar el LocalStorage
+    vi.spyOn(Storage.prototype, 'getItem').mockReturnValue('12345678');
+
     await TestBed.configureTestingModule({
       imports: [RegistrarObra],
+      providers: [
+        provideHttpClient(), 
+        provideRouter([])    
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(RegistrarObra);
     component = fixture.componentInstance;
+    
+    fixture.detectChanges(); 
     await fixture.whenStable();
   });
 
